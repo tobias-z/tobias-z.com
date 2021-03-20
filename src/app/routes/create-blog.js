dc1499613d0492d8d3b061a4533f104806edf242
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2021-03-20 20:11:26 +0100

    blog: ready for backend (title, description, blog) parameters

--- src/app/routes/create-blog.js
+++ src/app/routes/create-blog.js
@@ -1,11 +1,34 @@
 import * as React from "react"
-import {Button, Col, Container, Row} from "react-bootstrap"
+import {Button, Col, Container, Form, Row} from "react-bootstrap"
 import MDEditor from "@uiw/react-md-editor"
 
 function CreateBlog() {
+  const [title, setTitle] = React.useState("")
+  const [description, setDescription] = React.useState("")
   const [blog, setBlog] = React.useState("# Your blog")
+
   return (
     <Container>
+      <Row>
+        <Col>
+          <Form.Group controlId="blog-title">
+            <Form.Control
+              name="title"
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+              placeholder="Title"
+            />
+          </Form.Group>
+          <Form.Group controlId="blog-description">
+            <Form.Control
+              name="description"
+              value={description}
+              onChange={e => setDescription(e.target.value)}
+              placeholder="Description"
+            />
+          </Form.Group>
+        </Col>
+      </Row>
       <Row>
         <Col>
           <MDEditor value={blog} onChange={setBlog} />
@@ -16,7 +39,14 @@ function CreateBlog() {
           <Button
             className="w-25 p-2"
             onClick={() => {
-              // Do post with values
+              // Do post with blog + title
+              // Title has to be unique
+              const requestBody = {
+                title,
+                description,
+                blog,
+              }
+              console.log(requestBody)
             }}>
             Create Blog
           </Button>
